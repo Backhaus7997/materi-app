ae0110d558cab0fbe367d69ce7e5001495ae84f2
Backhaus <tinchoignacio33@gmail.com>
2025-12-02 10:55:02 -0300

    instalamos npm

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { 
